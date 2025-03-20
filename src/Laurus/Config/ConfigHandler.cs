0bb688f7292e03df0df3feb09e41c9125d5ef333
LunarLaurus <draknyte1@gmail.com>
2025-03-20 13:20:59 +0000

    Refactor logging property access in ConfigHandler
    - Changed `GLOBAL_LOGGING_ENABLED` and
      `GLOBAL_LOGGING_DEBUG_ENABLED` from readonly fields to
      properties in `ConfigHandler.cs`.
    - This change improves the flexibility of accessing the logging
      configuration by allowing dynamic evaluation of the option
      values instead of storing them as static fields.
    - The use of properties helps ensure that the latest values
      from configuration options are retrieved every time they are accessed.

--- src/Laurus/Config/ConfigHandler.cs
+++ src/Laurus/Config/ConfigHandler.cs
@@ -2,8 +2,8 @@ using XRL.UI;
 
 public static class ConfigHandler
 {
-    public static readonly bool GLOBAL_LOGGING_ENABLED = GetBooleanOption("OptionLaurusCoreLoggingEnabled");
-    public static readonly bool GLOBAL_LOGGING_DEBUG_ENABLED = GetBooleanOption("OptionLaurusCoreLoggingDebugEnabled");
+    public static bool GLOBAL_LOGGING_ENABLED => GetBooleanOption("OptionLaurusCoreLoggingEnabled");
+    public static bool GLOBAL_LOGGING_DEBUG_ENABLED => GetBooleanOption("OptionLaurusCoreLoggingDebugEnabled");
 
     private static bool GetBooleanOption(string optionKey) =>
         Options.GetOption(optionKey).EqualsNoCase("Yes");
